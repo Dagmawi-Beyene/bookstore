f920379e6a8f6fc4d6bc0003186c14e98ace88db
Dagmawi Beyene <dagmawi.beyene@salesframe.com>
2022-10-02 15:43:55 +0300

    Added restful services for books

--- src/main/java/spring22/bookstore/web/BookController.java
+++ src/main/java/spring22/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package spring22.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring22.bookstore.domain.*;
 
@@ -17,14 +21,22 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 
-	// all books
 	@RequestMapping(value = { "/", "/booklist" })
 	public String booklist(Model model) {
 		model.addAttribute("books", brepository.findAll());
 		return "booklist";
 	}
 
-	// Add new book
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> studentListRest() {
+		return (List<Book>) brepository.findAll();
+	}
+
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {
+		return brepository.findById(bookId);
+	}
+
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
@@ -32,14 +44,12 @@ public class BookController {
 		return "addbook";
 	}
 
-	// Save book
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
 		brepository.save(book);
 		return "redirect:booklist";
 	}
 
-	// Edit book
 	@RequestMapping(value = "/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", brepository.findById(bookId));
@@ -47,7 +57,6 @@ public class BookController {
 		return "editbook";
 	}
 
-	// Delete book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		brepository.deleteById(bookId);
